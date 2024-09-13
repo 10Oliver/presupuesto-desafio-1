9a5f2bb4b505d7bd222299ac41373396cbebec88
10Oliver <61370001+10Oliver@users.noreply.github.com>
2024-09-13 20:55:06 +0000

    🦺 Validations in form

--- views/transactionView.js
+++ views/transactionView.js
@@ -9,7 +9,9 @@ const formHtml = `
         <option value="2">Egreso</option>
     </select>
     <input type="text" class="input-form" id="description" placeholder="Descripción">
-    <input type="text" class="input-form" id="amount" placeholder="Monto">
+    <span id="description-message" class="alert"></span>
+    <input type="number" class="input-form" id="amount" placeholder="Monto">
+    <span id="amount-message" class="alert"></span>
     <button id="submit" onclick="submit()">Agregar</button>
 </div>
 `;
@@ -32,9 +34,41 @@ const submit = () => {
     const transactionType = document.getElementById("transaction-type-id").options[document.getElementById("transaction-type-id").selectedIndex];
     const amount = document.getElementById("amount");
     const description = document.getElementById("description");
+    // Validations
+    if (!showAlerts()) {
+        return;
+    }
     registerNewTransaction(transactionType.value, amount.value, description.value);
     // Clean
     document.getElementById("transaction-type-id").selectedIndex = 0;
     amount.value = "";
     description.value = "";
+}
+
+const showAlerts = () => {
+    let valid = true;
+    const number = Number(document.getElementById("amount").value);
+    const message = document.getElementById("amount-message");
+    const descriptionMessage = document.getElementById("description-message");
+    if (["", null].includes(document.getElementById("description").value)) {
+        descriptionMessage.innerHTML = "El campo es requerido";
+        valid = false;
+    }
+
+    if (number < 0 || number === 0) {
+        message.innerHTML = "El valor debe ser mayor a 0";
+        valid = false;
+    }
+
+    if (isNaN(number)) {
+        message.innerHTML = "El valor ingresado no es un número";
+        valid = false;
+    }
+
+    if (valid) {
+        descriptionMessage.innerHTML = "";
+        message.innerHTML = "";
+    }
+    
+    return valid;
 }
