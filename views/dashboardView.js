4132158f00c87d0074cb028a0b1a9e9f0d91773e
10Oliver <61370001+10Oliver@users.noreply.github.com>
2024-09-16 20:00:37 +0000

    ♻️ Change InnerHTML to nodes

--- views/dashboardView.js
+++ views/dashboardView.js
@@ -25,31 +25,85 @@ const loadDashboard = () => {
     // Get percentage
     const percentage = calculatePercentage(totalExpenses);
 
-    panel.innerHTML = `
-        <h6 class="title-date">
-            Presupuesto de ${capitalizedMonth} ${currentDate.getFullYear()}
-        </h6>
-        <span class="total-balance">
-            ${setSymbol(totalBalance)}
-        </span>
-        <div class="total-incomes general-results-container">
-            <span>Ingresos:</span>
-            <span class="total-incomes-value">
-                ${setSymbol(totalIncomes)}
-            </span>
-        </div>
-        <div class="total-expenses general-results-container">
-            <span>Egresos:</span>
-            <span class="total-expenses-value">
-                ${setSymbol(totalExpenses)}
-            </span>
-        </div>
-        <div class="percentage-container">
-            <span>Porcentaje de gastos:</span>
-            <div class="percentage-chip">
-                <span>${percentage ? percentage.toFixed(0) : '0'}%</span>
-            </div>
-            ${Math.abs(totalExpenses) >= totalIncomes && totalIncomes != 0 ? '<div class="warning-icon">!</div>' : ''}
-        </div>
-    `;;
+    // Clean panel
+    panel.textContent = ''; 
+
+    // Title
+    const title = document.createElement('h6');
+    title.classList.add('title-date');
+    title.textContent = `Presupuesto de ${capitalizedMonth} ${currentDate.getFullYear()}`;
+
+    // Total balance
+    const totalBalanceElement = document.createElement('span');
+    totalBalanceElement.classList.add('total-balance');
+    totalBalanceElement.textContent = setSymbol(totalBalance);
+
+    // Incomes sections
+    const totalIncomesContainer = document.createElement('div');
+    totalIncomesContainer.classList.add('total-incomes', 'general-results-container');
+
+    // Label
+    const incomesLabel = document.createElement('span');
+    incomesLabel.textContent = 'Ingresos:';
+    // Value
+    const totalIncomesValue = document.createElement('span');
+    totalIncomesValue.classList.add('total-incomes-value');
+    totalIncomesValue.textContent = setSymbol(totalIncomes);
+
+    // Include label and value into income section
+    totalIncomesContainer.appendChild(incomesLabel);
+    totalIncomesContainer.appendChild(totalIncomesValue);
+
+    // Expenses section
+    const totalExpensesContainer = document.createElement('div');
+    totalExpensesContainer.classList.add('total-expenses', 'general-results-container');
+
+    // Label
+    const expensesLabel = document.createElement('span');
+    expensesLabel.textContent = 'Egresos:';
+
+    // Value
+    const totalExpensesValue = document.createElement('span');
+    totalExpensesValue.classList.add('total-expenses-value');
+    totalExpensesValue.textContent = setSymbol(totalExpenses);
+
+    // Set label and value into expenses section
+    totalExpensesContainer.appendChild(expensesLabel);
+    totalExpensesContainer.appendChild(totalExpensesValue);
+
+    // Percentage section
+    const percentageContainer = document.createElement('div');
+    percentageContainer.classList.add('percentage-container');
+
+    // Label
+    const percentageLabel = document.createElement('span');
+    percentageLabel.textContent = 'Porcentaje de gastos:';
+
+    // Chip
+    const percentageChip = document.createElement('div');
+    percentageChip.classList.add('percentage-chip');
+
+    //Value
+    const percentageValue = document.createElement('span');
+    percentageValue.textContent = `${percentage ? percentage.toFixed(0) : '0'}%`;
+
+    // Label, chip and value into container
+    percentageChip.appendChild(percentageValue);
+    percentageContainer.appendChild(percentageLabel);
+    percentageContainer.appendChild(percentageChip);
+
+    // Show alert
+    if (Math.abs(totalExpenses) >= totalIncomes && totalIncomes !== 0) {
+        const warningIcon = document.createElement('div');
+        warningIcon.classList.add('warning-icon');
+        warningIcon.textContent = '!';
+        percentageContainer.appendChild(warningIcon);
+    }
+
+    // Include all childs into panel
+    panel.appendChild(title);
+    panel.appendChild(totalBalanceElement);
+    panel.appendChild(totalIncomesContainer);
+    panel.appendChild(totalExpensesContainer);
+    panel.appendChild(percentageContainer);
 }
