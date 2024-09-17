1d3049b1a0c09420ea7cca5a788789b5982adc1b
10Oliver <61370001+10Oliver@users.noreply.github.com>
2024-09-17 14:39:37 +0000

    ♻️ Finish move inner to node

--- components/transactionItem.js
+++ components/transactionItem.js
@@ -9,8 +9,30 @@ const drawItem = (name, value) => {
     itemDiv.appendChild(nameSpan);
 
     // Value
+    //const valueSpan = document.createElement('div');
+    //valueSpan.innerHTML = costValue(value);
+    // Value
     const valueSpan = document.createElement('div');
-    valueSpan.innerHTML = costValue(value);
+    // Verify for badget with percentage
+    if (value < 0) {
+        const badget = document.createElement('div');
+        badget.classList.add('badget');
+
+        const valueSpan = document.createElement('span');
+        valueSpan.textContent = setSymbol(value);
+
+        const percentageBadget = document.createElement('div');
+        percentageBadget.textContent = calculatePercentage(value).toFixed(0);
+
+        // Set values
+        badget.appendChild(valueSpan);
+        badget.appendChild(percentageBadget);
+        valueSpan.appendChild(badget)
+    } else {
+        const valueCost = document.createElement('span');
+        valueCost.textContent = setSymbol(value);
+        valueSpan.appendChild(valueCost);
+    }
     itemDiv.appendChild(valueSpan);
 
     return itemDiv;
@@ -20,7 +42,7 @@ const setTransactionItems = (list) => {
     const listPanel = document.getElementById("transaction-list");
 
     // Clean container
-    listPanel.textContent = ''; 
+    listPanel.textContent = '';
 
     // Include item
     list.forEach((item) => {
